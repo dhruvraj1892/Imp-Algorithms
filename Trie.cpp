d205c50263707c67dcbc9f099eaedd25bf0428d7
Dhruv Raj <dhruvrajdo005@gmail.com>
2026-07-29 21:48:42 +0530

    second commit

---
+++ Trie.cpp
@@ -0,0 +1,44 @@
+struct Node{
+    Node *links[26];
+    bool flag=false;
+ 
+    bool containsKey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    void put(char ch,Node *node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+}
+class Trie{
+    private: Node* root;
+    public:
+    Trie(){
+        root=new Node();
+    } 
+    void insert(string word){
+        Node *node=root;
+        for(char c:word){
+         if(!node->containsKey(c)) node->put(c,new Node());
+        node= node->get(c);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word){
+        Node *node =root;
+        for(char c:word){
+        if(!node->containsKey(c)) return false;
+        node=node->get(c);
+        }
+        return node->isEnd();
+    }
+}
