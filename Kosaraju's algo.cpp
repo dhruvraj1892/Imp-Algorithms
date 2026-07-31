f753cf68589d9b71a23b9df75a01508da54439ee
Dhruv Raj <dhruvrajdo005@gmail.com>
2026-08-01 00:36:12 +0530

    sixth commit

---
+++ Kosaraju's algo.cpp
@@ -0,0 +1,6 @@
+//used to find the count of strongly connected components in a directed graph which may contains cycles 
+// first topo sort the graph using stack and dfs
+// reverse all the edges of the graph  
+// start with the top-most vertex in the stack and run dfs and count the components accordingly
+
+
